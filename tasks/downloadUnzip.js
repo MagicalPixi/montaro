887554f9a6e542107ada09df9b3147da507e2a6c
zhou-yg <zhou.y.g890@gmail.com>
2016-08-25 23:13:56 +0800

    fix.

--- tasks/downloadUnzip.js
+++ tasks/downloadUnzip.js
@@ -17,7 +17,7 @@ var unzipDownload = require('./lib/unzipDownload');
 
 var packageJsonPath = path.resolve(__dirname,'../package.json');
 
-var server = __DEBUG__ ? 'http://localhost:1414/' :  'http://www.magicalpixi.com/';
+var server = __DEBUG__ ? 'http://localhost:1414/' :  'http://back.magicalpixi.com/';
 //资源名字
 var downloadMaterialNames = [];
 //var downloadMaterialNames = [
