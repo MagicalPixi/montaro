cb63ab4e8feb95b56c13d178ec735255a994e72e
guoshencheng <648772021@qq.com>
2016-09-14 11:45:09 +0800

    start with game

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 var pixiLib = require('pixi-lib')
 var render = pixiLib.createRender(document.body)
-var end = require('./end')
 var game = require('./game')
 
-// game(render)
-
-end(render, 300, true)
+game(render)
