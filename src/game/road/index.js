9b74753cfdce07c92556bc6de5e2f8a8e28ff16a
zhou-yg <zhou.y.g890@gmail.com>
2016-09-21 21:57:00 +0800

    优化背景，防止一次生成爆掉。

--- src/game/road/index.js
+++ src/game/road/index.js
@@ -13,7 +13,7 @@ function repeatBackground(spriteFn) {
   var arr = [];
 
 
-  var fns = [].slice.call(arguments,0,arguments.length);
+  var fns = [].slice.call(arguments, 0, arguments.length);
 
   var num = fns.length;
 
@@ -23,16 +23,16 @@ function repeatBackground(spriteFn) {
 
     bg.x = bg.initX = i * 1004;
     bg.y = 640 - bg.height;
-    bg.sideX = -(num-i) * 1004;
+    bg.sideX = -(num - i) * 1004;
 
     return bg;
   }
 
-  for(var i=0;i<num;i++){
+  for (var i = 0; i < num; i++) {
 
     var fn = fns[i];
 
-    var bg = build(fn,i);
+    var bg = build(fn, i);
 
     arr.push(bg);
   }
@@ -41,6 +41,21 @@ function repeatBackground(spriteFn) {
 
 
   return arr;
+}
+
+
+function getBg(bgFn, i) {
+
+  if (i === undefined) {
+    i = 1;
+  }
+
+  var bg = bgFn();
+
+  bg.x = bg.initX = i * 1004;
+  bg.sideX = -1 * 1004;
+
+  return bg;
 }
 
 
@@ -54,7 +69,18 @@ bgs.map(function (bg) {
 });
 
 
-var buildings = repeatBackground(
+// var buildings = repeatBackground(
+//   groupFn,
+//   groupFn2,
+//   groupFn,
+//   groupFn2,
+//   groupFn4,
+//   groupFn,
+//   groupFn2,
+//   groupFn3,
+//   groupFn,
+//   groupFn2);
+var buildingsArr = [
   groupFn,
   groupFn2,
   groupFn,
@@ -64,25 +90,64 @@ var buildings = repeatBackground(
   groupFn2,
   groupFn3,
   groupFn,
-  groupFn2);
+  groupFn2
+];
+//
+// buildings.map(function (b) {
+//   b.y = 0;
+//   stage.addChild(b);
+// })
 
-buildings.map(function (b) {
-  b.y = 0;
-  stage.addChild(b);
-})
 
+stage.buildings = [
+  getBg(buildingsArr[0], 0),
+  getBg(buildingsArr[1]),
+];
+
+stage.addChild(stage.buildings[0])
+stage.addChild(stage.buildings[1])
+
+buildingsArr = buildingsArr.slice(2);
 
 stage.setSpeed = function (s) {
   stage.speed = s;
 }
 
 stage.render = function () {
-  [].concat(bgs).concat(buildings).map(function (bg) {
+
+  var newBuildings = [];
+
+  stage.buildings.map(function (bg) {
+
+    if (bg.removed) {
+      return;
+    }
+
     bg.x -= stage.speed;
-    if(bg.x <= bg.sideX){
-      bg.x = bg.initX;
+
+    if (bg.x <= bg.sideX) {
+
+      bg.removed = true;
+      stage.removeChild(bg);
+
+    } else {
+      newBuildings.push(bg);
     }
   });
+
+  if (newBuildings.length < 2 && buildingsArr.length > 0) {
+
+    var newBg = getBg(buildingsArr[0]);
+
+    newBuildings.push(newBg)
+
+    stage.addChild(newBg);
+
+    buildingsArr = buildingsArr.slice(1);
+
+    stage.buildings = newBuildings.slice();
+  }
+
 }
 
 module.exports = stage;
